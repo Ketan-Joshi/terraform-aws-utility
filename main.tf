e544dbe4d3f3dc9bbeb5b37244c5a9f71d2892d3
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-06 13:45:30 +0530

    Added Utility Servers

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "template_file" "userdata" {
   }
 }
 resource "aws_instance" "utility" {
-  ami = "ami-0b5eea76982371e91"
+  ami = "ami-09cd747c78a9add63"
   instance_type = var.instance_type_utility
   user_data = data.template_file.userdata.rendered
   key_name = var.pem_key_name
